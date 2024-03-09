69f53574da286b8ddff6b3f055f7261c2d96fad6
daniyelrub <daniyalruban1999@gmail.com>
2024-03-08 14:29:49 -1200

    dayseven

---
+++ js/day7.js
@@ -0,0 +1,26 @@
+//country data
+const req=new XMLHttpRequest();
+
+Request.open('GET','https://restcountries.com/v3.1/all' )
+req.send()
+
+req.onload=function() {
+
+const jsonArray = JSON.parse(this.response)
+
+console.log("print the country that uses.us dollars as a country")
+{
+if(countries.countries?.USD)
+{
+console.log("country Name:",countries.name.common)
+
+
+
+}
+
+
+}
+
+
+
+}
---
+++ js/jay.js
@@ -0,0 +1,4 @@
+//all country link
+https://restcountries.com/v3.1/all
+
+
